a7f063e8d4d97b3a7afb86e31dd42634e19787f8
M <miczdevelopercontact@gmail.com>
2022-08-19 00:23:19 +0200

    reformating hw

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ class My_class():
         pass
     def say_hello_world(self):
         print('Hello world')
+    
+    def say_hi(self):
+        print('Hi, how are you?')
 
 my_instance = My_class()
 
