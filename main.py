62ae811527195f9499db4584d0f9f03615f511ad
M <miczdevelopercontact@gmail.com>
2022-08-19 00:14:26 +0200

    reformated_hw

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world')
+#This is just an starting point
+class My_class():
+    def __init__(self):
+        pass
+    def say_hello_world(self):
+        print('Hello world')
+
+my_instance = My_class()
+
+my_instance.say_hello_world()
